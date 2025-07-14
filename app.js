5bb6b0e5635a360d941ced2cd1b1e999359b7f75
deepakkrmaurya <deepakmaurya2211@gmail.com>
2025-07-14 11:08:04 +0530

    DDDD

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors({
     // origin:'http://localhost:5173',
     origin:'https://doctor-appointment-client-gray.vercel.app',
     credentials:true,
-    optionsSuccessStatus:true
+    // optionsSuccessStatus:true
 }))
 
 
