325be5f31987bdea3fd531139173675ce6438248
deepakkrmaurya <deepakmaurya2211@gmail.com>
2025-09-21 17:55:41 +0530

    jdgshsjdh

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 // ✅ Socket.io setup
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://hummarichikitsa.vercel.app",
         methods: ["GET", "POST"],
     },
 });
@@ -26,19 +26,6 @@ const io = new Server(server, {
 // ✅ Listen for connections
 io.on("connection", (socket) => {
     console.log("⚡ New client connected:", socket.id);
-    //update appointment
-    // socket.on("update_appointmaet", async (data) => {
-    //     try {
-    //         const { appointment_id } = data
-    //         const i = await apponitment.findByIdAndUpdate(appointment_id, { status: "completed" });
-    //         const appointment = await apponitment.find({ patientId: i.patientId });
-    //         console.log(appointment.length)
-    //         io.emit("update_appointmaet", appointment);
-    //         // completed
-    //     } catch (error) {
-    //         socket.emit("update_error", error.message);
-    //     }
-    // });
 
     socket.on("disconnect", () => {
         console.log("❌ Client disconnected:", socket.id);
